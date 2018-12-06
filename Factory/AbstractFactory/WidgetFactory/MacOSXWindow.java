95b1b82bd51ef22543a0a77d49fdcf37b51af811
Travis Cooper <dozertdc@gmail.com>
2018-12-06 08:46:54 -0500

    Create MacOSXWindow.java

---
+++ Factory/AbstractFactory/WidgetFactory/MacOSXWindow.java
@@ -0,0 +1,10 @@
+
+//ConcreteProductA2
+public class MacOSXWindow implements Window{
+  public void setTitle(){
+    //Mac OSX specific behaviour
+  }
+  public void repaint(){
+    //Mac OSX specific behaviour
+  }
+}
