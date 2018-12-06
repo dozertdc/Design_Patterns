8ba2e49d0d1e483e71e12e26895fa53500190ef5
Travis Cooper <dozertdc@gmail.com>
2018-12-06 08:46:36 -0500

    Create MSWindow.java

---
+++ Factory/AbstractFactory/WidgetFactory/MSWindow.java
@@ -0,0 +1,9 @@
+//ConcreteProductA1
+public class MSWindow implements Window{
+  public void setTitle(){
+    //MS Windows specific behaviour
+  }
+  public void repaint(){
+    //MS Windows specific behaviour
+  }
+}
