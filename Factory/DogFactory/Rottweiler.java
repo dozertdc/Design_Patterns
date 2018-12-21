a62f052726a6fdbc075bc58b5807599d9c2fa555
Travis Cooper <dozertdc@gmail.com>
2018-12-21 09:34:56 -0500

    Update Rottweiler.java

--- Factory/DogFactory/Rottweiler.java
+++ Factory/DogFactory/Rottweiler.java
@@ -1,7 +1,9 @@
+// Rottweiler type
 class Rottweiler implements Dog
 {
   public void speak()
   {
+    // unique Rottweiler Woof
     System.out.println("The Rottweiler says (in a very deep voice) \"WOOF!\"");
   }
 }
